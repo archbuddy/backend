3870d0b6f242d6c670581d3caf511fc328b885df
Bruno Penso <bruno.penso@gmail.com>
2022-04-28 21:48:58 -0300

    patch nodes

--- tests/nodes.test.js
+++ tests/nodes.test.js
@@ -21,3 +21,16 @@ test('delete node', async function () {
   expect((await srv.getEdges()).length).toBe(1)
   expect((await srv.getNodes()).length).toBe(3)
 })
+
+test('patch nodes', async function () {
+  const obj = {
+    id: 'n1',
+    position: { x: 900, y: 200 }
+  }
+  await srv.patchNode([obj])
+
+  const nodes = await srv.getNodes()
+  const index = nodes.findIndex((element) => element.id === obj.id)
+  expect(nodes[index].position.x).toBe(900)
+  expect(nodes[index].position.y).toBe(200)
+})
