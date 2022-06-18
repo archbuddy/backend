ced1810a22cb11fe0805fe2c527c8bf91a435e25
Bruno Penso <bruno.penso@gmail.com>
2022-06-18 18:43:11 -0300

    remove description duplication

--- src/route/edge.js
+++ src/route/edge.js
@@ -3,6 +3,7 @@ const edgeController = require('../controller/edge.js')
 const { getPageSchema } = require('../swagger.js')
 const { edgeUpsertSchema } = require('../schema/edge.js')
 const { diagramItemSchema } = require('../schema/diagramItem.js')
+const { fiqlDescription } = require('./base')
 
 const list = {
   preValidation,
@@ -21,30 +22,7 @@ const list = {
           default: ''
         },
         fiql: {
-          description: `field you can define complex queries to filter objects. 
-            __All object and sub-object properties are available to be used in your query__.
-
-#### Example 
-Request: '/entites?fiql=active==true;name==CPLV'
-
-#### Combinations
-Operator | Description
---- | ---
-; | AND
-, | OR
-()| Parentheses can be used define precedence
-
-
-#### Logicals
-Operator | Description
---- | ---
-== | Equal To
-!= | Not Equal To
-=gt= | Greater Than
-=ge= | Greater Than or Equal To 
-=lt= | Less Than
-=le= | Less Than or Equal To 
-=re= | Regular expressions. Available only for 'string' properties. <br /><br />Format: '('<regular expression>','<options: optional>')' <br /><br /> See details in [mongoDb docs](https://docs.mongodb.com/manual/reference/operator/query/regex/).`,
+          description: fiqlDescription,
           type: 'string',
           in: 'query',
           default: ''
