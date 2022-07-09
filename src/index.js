fbd24a49a76650211141918f62b62d0165d3eaea
Bruno Penso <bruno.penso@gmail.com>
2022-07-09 14:25:55 -0300

    lint

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ const log = require('./util/logger')
 const fastify = require('fastify')({ logger: false })
 
 const { connectMongo, disconnectMongo } = require('./repository/db.js')
-const { setup, setupRegisters, setupHooks, setupSchemas, registryCommonRoutes, registrySimpleRoute } = require('./fastify')
+const { setupRegisters, setupHooks, setupSchemas, registryCommonRoutes, registrySimpleRoute } = require('./fastify')
 
 // Run the server!
 const start = async () => {
@@ -16,13 +16,13 @@ const start = async () => {
   registryCommonRoutes(fastify, '/diagrams', require('./route/diagram'))
   registryCommonRoutes(fastify, '/edges', require('./route/edge'))
   registryCommonRoutes(fastify, '/nodes', require('./route/node'))
-  
+
   registrySimpleRoute(fastify, '/healthcheck', require('./route/health').check, 'GET')
   registrySimpleRoute(fastify, '/healthcheck/complete', require('./route/health').complete, 'GET')
   registrySimpleRoute(fastify, '/diagrams/:id/reactflow', require('./route/diagram').reactFlow, 'GET')
   registrySimpleRoute(fastify, '/authentication/google', require('./route/auth').authentication, 'POST')
   registrySimpleRoute(fastify, '/authentication/providers', require('./route/auth').providers, 'GET')
-  
+
   await fastify.ready()
   try {
     log.info('Connecting to mongo')
